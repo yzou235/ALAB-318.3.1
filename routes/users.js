2b9db918b4bee86c7c81f66aa221149c0273a469
yzou235 <yumingzou235@gmail.com>
2024-01-13 20:11:11 -0500

    Added comments require to user.js and posts.js in routes

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const users = require("../data/users");
 const posts = require("../data/posts");
+const comments = require("../data/comments");
 const error = require("../utilities/error");
 
 router
